617acc301d15b6eb211cadde4eba5d62419b3d66
Cozy <csande9@clemson.edu>
2025-08-26 15:04:25 -0400

    Moved db initialization to within the job asyncio event loop, not a worker asyncio event loop

--- app/worker.py
+++ app/worker.py
@@ -12,16 +12,12 @@ listen = ['high', 'default', 'low']
 
 REDIS_URL = os.getenv("REDIS_TLS_URL") or os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
-async def _startup():
-    await init_db()
-
 def main():
     if REDIS_URL.startswith("rediss://"):
         conn = redis.from_url(REDIS_URL, ssl_cert_reqs=ssl.CERT_NONE)
     else:
         conn = redis.from_url(REDIS_URL)
         
-    asyncio.run(_startup())
     with Connection(conn):
         worker = Worker([Queue(name) for name in listen])
         worker.work(with_scheduler=True)

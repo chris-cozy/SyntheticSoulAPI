901c4fa321e70cbf1040cd670e4675b503e7389f
Cozy <csande9@clemson.edu>
2025-08-26 14:50:21 -0400

    Initialize database on startup

--- app/worker.py
+++ app/worker.py
@@ -1,12 +1,19 @@
 import os
-
+import asyncio
 import redis
 import ssl
 from rq import Worker, Queue, Connection
+from dotenv import load_dotenv
+from app.services.data_service import init_db
+
+load_dotenv()
 
 listen = ['high', 'default', 'low']
 
 REDIS_URL = os.getenv("REDIS_TLS_URL") or os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+async def _startup():
+    await init_db()
 
 def main():
     if REDIS_URL.startswith("rediss://"):
@@ -14,6 +21,7 @@ def main():
     else:
         conn = redis.from_url(REDIS_URL)
         
+    asyncio.run(_startup())
     with Connection(conn):
         worker = Worker([Queue(name) for name in listen])
         worker.work(with_scheduler=True)

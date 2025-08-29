c68d019213a8d9d01d1bca346b4549577e2951b9
Cozy <csande9@clemson.edu>
2025-08-29 01:39:10 -0400

    Ensure indexes

--- app/services/data_service.py
+++ app/services/data_service.py
@@ -39,6 +39,21 @@ def _client_opts() -> Dict[str, Any]:
         # "tls": True, "tlsAllowInvalidCertificates": False,  # if applicable
     }
 
+async def _ensure_indexes(db):
+    await db[AGENT_LITE_COLLECTION].create_index("name", unique=True)
+    await db[AGENT_COLLECTION].create_index("name", unique=True)
+    await db[USER_LITE_COLLECTION].create_index(
+        [("username", 1), ("agent_perspective", 1)],
+        unique=True,
+        name="username_agent_perspective"
+    )
+    await db[USER_COLLECTION].create_index("username", unique=True)
+    await db[CONVERSATION_COLLECTION].create_index(
+        [("username", 1), ("agent_name", 1)],
+        name="username_agent"
+    )
+    await db[MESSAGE_MEMORY_COLLECTION].create_index("agent_name", name="agent_name")
+
 db_client = None
 
 async def init_db() -> None:
@@ -149,6 +164,8 @@ async def _initialize_collections(client: AsyncIOMotorClient) -> None:
     if update_tasks:
         await asyncio.gather(*update_tasks)
     update_time = time.perf_counter() - t_update
+    
+    await _ensure_indexes(db)
     
     print(
         "Collection init: "
